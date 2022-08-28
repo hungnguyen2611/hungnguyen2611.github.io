4e4cabb8bbaacbd81e5835f1125f3277079bf27e
hungnguyen2611 <hung.nguyenantslayer@hcmut.edu.vn>
2022-08-28 14:46:48 -0700

    Updates

---
+++ precache-manifest.516f3a91a7c0006a76422cabe018c273.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5107b8259fd1bc00ae6d517d034775a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2950d1df9269dde6c684",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "c7dea3e979af6434230b",
+    "url": "/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "d65bcc18a7d039104811",
+    "url": "/static/css/main.1a6005f5.chunk.css"
+  },
+  {
+    "revision": "c642830d4771042549c4",
+    "url": "/static/js/2.7f4bbd56.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.7f4bbd56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2950d1df9269dde6c684",
+    "url": "/static/js/3.e61f7160.chunk.js"
+  },
+  {
+    "revision": "c7dea3e979af6434230b",
+    "url": "/static/js/4.58100f00.chunk.js"
+  },
+  {
+    "revision": "d65bcc18a7d039104811",
+    "url": "/static/js/main.fbf7a35b.chunk.js"
+  },
+  {
+    "revision": "5a7c90d40e06b31f02b1",
+    "url": "/static/js/runtime-main.b19d5ff2.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "d286639e54eb3ae6f38f106c7bafb433",
+    "url": "/static/media/BKLogo.d286639e.png"
+  },
+  {
+    "revision": "45727ab4099f3750182d0afba78f0bef",
+    "url": "/static/media/DNN.45727ab4.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "2fa33e5fe83b1e8f63937adb25d9605b",
+    "url": "/static/media/emageLogo.2fa33e5f.png"
+  },
+  {
+    "revision": "e5dd27aeee12f51da29803c3285be45b",
+    "url": "/static/media/face_reg.e5dd27ae.png"
+  },
+  {
+    "revision": "63972f35ab3b581195f17906adc673ac",
+    "url": "/static/media/idp_ielts.63972f35.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
